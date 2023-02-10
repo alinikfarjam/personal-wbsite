948381f1aa27194efa31da1d8404a9509bc000b6
ali niki <alinikfarjam344@gmail.com>
2023-02-10 10:43:03 +0330

    fix bugs responsive sample work section

--- src/sample-work/photoShop/SamplePhotoShop.js
+++ src/sample-work/photoShop/SamplePhotoShop.js
@@ -3,12 +3,22 @@ import logo from "../../assets/image/logo.png"
 
 const SamplePhotoShop = () => {
     return (
-        <section className="SamplePhotoShop_Item">
-
-            <div>
-                <img src={logo} />
-
-            </div>
+        <section className="SamplePhotoShop_container">
+            <section className="SamplePhotoShop_Item">
+                <div>
+                    <img src={logo} />
+                </div>
+            </section>
+            <section className="SamplePhotoShop_Item">
+                <div>
+                    <img src={logo} />
+                </div>
+            </section>
+            <section className="SamplePhotoShop_Item">
+                <div>
+                    <img src={logo} />
+                </div>
+            </section>
         </section>
     );
 }
